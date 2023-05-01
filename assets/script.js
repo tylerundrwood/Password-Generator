7c6012c86973a8032b1c63fbd1ee923d6580b494
Tyler Underwood <tylerunderwood@Tylers-Laptop.lan>
2023-05-01 18:20:49 -0400

    started by adding function and character list

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,37 @@
 // Assignment code here
+function randomCharacter(list) {
+  console.log("Button pressed!");
+  return list[randomInteger(list.length)];
+
+  function randomInteger(min, max) {
+    if (!max) {
+    max = min;
+    min = 0;
+  } 
+  let character = Math.random()
+  return Math.floor(min*(1 - character) + character * max)
+}
+function generatePassword() {
+let userChoice = prompt("Create a randomized character password")
+let length = (8)<(128);
+let alpha = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+let numeric = "0123456789";
+let symbol = "!@#$%^&*()_+~|}{[]?></-=";
+let password = "";
+
+
+return
+  }
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+ 
   passwordText.value = password;
 
 }
