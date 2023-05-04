632ed0c6c12d58785289ad5c001e62d955e9e2d1
Tyler Underwood <twood9600@yahoo.com>
2023-05-04 16:55:25 -0400

    added copy function

--- assets/script.js
+++ assets/script.js
@@ -81,3 +81,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function copyFunction() {
+  // Get the text field
+  let copyText = document.getElementById("password");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+
+  document.execCommand('copy')
+}
+
+
+
+
+
+  
+
+
+
+
+
+
+ 
+ 
